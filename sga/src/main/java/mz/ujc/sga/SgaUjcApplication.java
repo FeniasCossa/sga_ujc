86cf0a1933a882dd625d78b75b9bdd3ff371091a
FeniasCossa <fenisasebastiao636@gmail.com>
2023-05-20 22:57:46 +0200

    first commit

---
+++ sga/src/main/java/mz/ujc/sga/SgaUjcApplication.java
@@ -0,0 +1,13 @@
+package mz.ujc.sga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaUjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaUjcApplication.class, args);
+	}
+
+}
---
+++ sga/src/test/java/mz/ujc/sga/SgaUjcApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.ujc.sga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaUjcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
